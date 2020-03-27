28388211942f43d151cd696b108a66c81821dbc7
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-03-27 23:08:39 +0100

    String play

---
+++ string_play.py
@@ -0,0 +1,15 @@
+#--
+# ToDos
+# Create the text from the msg variable: "1 Welcome Ring To Tyler"
+# Reverse the text created
+
+msg='welcome to Python 101: Strings'
+num = msg[20];
+welcome_text = msg[:7];
+ring_text = msg[-5:-1]
+to_text = msg[8:10]
+name = msg[8]+msg[12]+msg[2]+msg[1]+msg[-5]
+
+sentence = num + ' ' + welcome_text + ' ' + ring_text + ' ' + to_text + ' ' + name
+# print(sentence.title())
+print(sentence[::-1]);
