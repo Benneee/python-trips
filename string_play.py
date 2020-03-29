98bfcf5e03447fac5709c2161ced54cbabd4b929
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-03-29 14:29:18 +0100

    More python string play

--- string_play.py
+++ string_play.py
@@ -12,4 +12,6 @@ name = msg[8]+msg[12]+msg[2]+msg[1]+msg[-5]
 
 sentence = num + ' ' + welcome_text + ' ' + ring_text + ' ' + to_text + ' ' + name
 # print(sentence.title())
+
+# reversing the text
 print(sentence[::-1]);
